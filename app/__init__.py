9ceb71de867b9f9f1ef18b9d6679f891f75a6521
AnnaZapototska <zapototskaann@gmail.com>
2023-06-04 00:26:16 +0300

    add

--- app/__init__.py
+++ app/__init__.py
@@ -93,6 +93,7 @@ def create_app():
     @app.route('/ytmusic/callback')
     def handle_ytmusic_callback():
         code = request.args.get('code')
+        print("Start")
 
         # Exchange the authorization code for an access token
         token_url = 'https://oauth2.googleapis.com/token'
@@ -103,11 +104,13 @@ def create_app():
             'code': code,
             'grant_type': 'authorization_code',
         }
+        print("check 1")
         response = requests.post(token_url, data=token_params)
         if response.status_code == 200:
             token_data = response.json()
             access_token = token_data['access_token']
             refresh_token = token_data['refresh_token']
+            print("check 2")
 
             # Get the user's ID from the access token
             user_info_url = 'https://www.googleapis.com/oauth2/v1/userinfo'
@@ -116,13 +119,17 @@ def create_app():
             user_info_data = response.json()
             print("User data", user_info_data)
             user_id = user_info_data['id']
+            print("check 3")
 
             # Store the access token, refresh token, and user ID in the session
             session['access_token'] = access_token
             session['refresh_token'] = refresh_token
             session['user_id'] = user_id
 
+            print("check 4")
+
             return redirect(url_for('show_ytmusic_playlists'))
+
         else:
             return "Error occurred during authentication."
 
