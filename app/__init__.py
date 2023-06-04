6dd6a2fbab62452c21bd773c30fb4137e1baaf35
AnnaZapototska <zapototskaann@gmail.com>
2023-06-04 22:48:03 +0300

    add

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 import secrets
+import urllib
+from urllib import parse
 
+import requests
 from flask import Flask, redirect, url_for, session, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from ytmusicapi import YTMusic
 import json
-from app.oauth import authenticate_ytmusic, handle_ytmusic_callback
 
 db = SQLAlchemy()
 ytmusic = None  # Global variable to hold the YTMusic client object
@@ -73,76 +75,69 @@ def create_app():
             traceback.print_exc()
             return "An error occurred while retrieving playlists. Please try again later."
 
-    # @app.route('/ytmusic/auth')
-    # def authenticate_ytmusic():
-    #     # Redirect the user to YouTube for authentication
-    #     auth_url = 'https://accounts.google.com/o/oauth2/auth'
-    #     params = {
-    #         'client_id': '866143699543-g80lda2kbtp9ci0gskh3em31vvf2t0l0.apps.googleusercontent.com',
-    #         'redirect_uri': request.url_root + 'ytmusic/callback',
-    #         'scope': 'https://www.googleapis.com/auth/youtube.force-ssl',
-    #         'response_type': 'code',
-    #         'access_type': 'offline',
-    #         'prompt': 'consent',
-    #     }
-    #     auth_redirect = auth_url + '?' + urllib.parse.urlencode(params)
-    #
-    #     # Generate and set a unique session ID (cookie value)
-    #     session['session_id'] = generate_session_id()
-    #
-    #     return redirect(auth_redirect)
-    #
-    # @app.route('/ytmusic/callback')
-    # def handle_ytmusic_callback():
-    #     code = request.args.get('code')
-    #
-    #     # Exchange the authorization code for an access token
-    #     token_url = 'https://oauth2.googleapis.com/token'
-    #     token_params = {
-    #         'client_id': '866143699543-g80lda2kbtp9ci0gskh3em31vvf2t0l0.apps.googleusercontent.com',
-    #         'client_secret': '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',
-    #         'redirect_uri': 'https://my-playlist-project.onrender.com/ytmusic/callback',
-    #         'code': code,
-    #         'grant_type': 'authorization_code',
-    #     }
-    #
-    #     response = requests.post(token_url, data=token_params)
-    #
-    #     if response.status_code == 200:
-    #         token_data = response.json()
-    #         access_token = token_data['access_token']
-    #         refresh_token = token_data['refresh_token']
-    #
-    #         # Get the user's ID from the access token
-    #         user_info_url = 'https://www.googleapis.com/oauth2/v1/userinfo'
-    #         headers = {'Authorization': 'Bearer ' + access_token}
-    #         response = requests.get(user_info_url, headers=headers)
-    #         user_info_data = response.json()
-    #         print("User data", user_info_data)
-    #
-    #         if 'id' in user_info_data:
-    #             user_id = user_info_data['id']
-    #             print("User ID:", user_id)
-    #             # Store the access token, refresh token, and user ID in the session
-    #             session['access_token'] = access_token
-    #             session['refresh_token'] = refresh_token
-    #             session['user_id'] = user_id
-    #
-    #             print("check 3")
-    #
-    #             return redirect(url_for('show_ytmusic_playlists'))
-    #         else:
-    #             return "User ID not found in response."
-    #     else:
-    #         return "Error occurred during authentication."
-
     @app.route('/ytmusic/auth')
-    def ytmusic_auth():
-        return authenticate_ytmusic()
+    def authenticate_ytmusic():
+        # Redirect the user to YouTube for authentication
+        auth_url = 'https://accounts.google.com/o/oauth2/auth'
+        params = {
+            'client_id': '866143699543-g80lda2kbtp9ci0gskh3em31vvf2t0l0.apps.googleusercontent.com',
+            'redirect_uri': request.url_root + 'ytmusic/callback',
+            'scope': 'https://www.googleapis.com/auth/youtube.force-ssl',
+            'response_type': 'code',
+            'access_type': 'offline',
+            'prompt': 'consent',
+        }
+        auth_redirect = auth_url + '?' + urllib.parse.urlencode(params)
+
+        # Generate and set a unique session ID (cookie value)
+        session['session_id'] = generate_session_id()
+
+        return redirect(auth_redirect)
 
     @app.route('/ytmusic/callback')
-    def ytmusic_callback():
-        return handle_ytmusic_callback()
+    def handle_ytmusic_callback():
+        code = request.args.get('code')
+
+        # Exchange the authorization code for an access token
+        token_url = 'https://oauth2.googleapis.com/token'
+        token_params = {
+            'client_id': '866143699543-g80lda2kbtp9ci0gskh3em31vvf2t0l0.apps.googleusercontent.com',
+            'client_secret': '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',
+            'redirect_uri': 'https://my-playlist-project.onrender.com/ytmusic/callback',
+            'code': code,
+            'grant_type': 'authorization_code',
+        }
+
+        response = requests.post(token_url, data=token_params)
+
+        if response.status_code == 200:
+            token_data = response.json()
+            access_token = token_data['access_token']
+            refresh_token = token_data['refresh_token']
+
+            # Get the user's ID from the access token
+            user_info_url = 'https://www.googleapis.com/oauth2/v1/userinfo'
+            headers = {'Authorization': 'Bearer ' + access_token}
+            response = requests.get(user_info_url, headers=headers)
+            print("User data", requests.get(user_info_url, headers=headers))
+            user_info_data = response.json()
+            print("User data", response)
+
+            if 'id' in user_info_data:
+                user_id = user_info_data['id']
+                print("User ID:", user_id)
+                # Store the access token, refresh token, and user ID in the session
+                session['access_token'] = access_token
+                session['refresh_token'] = refresh_token
+                session['user_id'] = user_id
+
+                print("check 3")
+
+                return redirect(url_for('show_ytmusic_playlists'))
+            else:
+                return "User ID not found in response."
+        else:
+            return "Error occurred during authentication."
 
     @app.teardown_appcontext
     def teardown_appcontext(error):
